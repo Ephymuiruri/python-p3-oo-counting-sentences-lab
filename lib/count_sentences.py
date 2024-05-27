520389255b2ced3a3e0f035d8171942b08fa6cb8
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-27 08:39:19 +0300

    All tests passing

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value  
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+        self._value = value
+
+    value = property(get_value, set_value)
+
+    def is_sentence(self):
+        if self._value and self._value[-1] == ".":
+            return True
+        return False
+    
+    def is_question(self):
+        if self._value and self._value[-1] == "?":
+            return True
+        return False
+    def is_exclamation(self):
+        if self._value and self._value[-1] =="!":
+            return True
+        return False
+    def count_sentences(self):
+        sentence_pattern = r'[A-Za-z0-9,;\s]+[.!?]'
+        sentences = re.findall(sentence_pattern, self._value)
+        print(sentences)
+        sentences =[s for s in sentences if s.strip()]
+        return len(sentences)
